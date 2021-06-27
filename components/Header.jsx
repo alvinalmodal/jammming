bbe5f334a3582b808534d7e5193326bacb8a890b
alvin almodal <almodalalvin@gmail.com>
2021-06-27 13:08:43 +0800

    added header component

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <h1 className={styles.header}>
+      Ja<span>mmm</span>ing
+    </h1>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -10,11 +11,12 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap"
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header></Header>
     </div>
   );
 }
